25ffb0e4238a3d1c2222d3d91382220cc54fcc09
Michał Kamiński <32871390+michal-b-kaminski@users.noreply.github.com>
2023-08-23 23:36:16 +0200

    Preparation for release (#4)
    * chore(*BREAKING*): refactoring, removed interface and provide only concrete client class
    
    * chore: enable generation of XML documentation file
    
    * chore: rename workflows in GitHub actions
    
    * chore: add missing comment

--- TimeTaggerClient/TimeTaggerClient.cs
+++ TimeTaggerClient/TimeTaggerClient.cs
@@ -1,18 +1,28 @@
 ﻿using System.Text;
 using System.Text.Json;
 
-using TimeTaggerClient.Interfaces;
 using TimeTaggerClient.Models;
 
 namespace TimeTaggerClient
 {
-    public class TimeTaggerClient : ITimeTaggerClient, IDisposable
+    /// <summary>
+    /// TimeTagger API wrapper.
+    /// </summary>
+    public class TimeTaggerClient : IDisposable
     {
         private bool disposedValue;
 
+        /// <summary>
+        /// Full URL of TimeTagger API, with end /, eg. http://localhost/timetagger/api/v2/.
+        /// </summary>
         public string Server { get; set; }
         internal HttpClient HttpClient { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimeTaggerClient"/> class.
+        /// </summary>
+        /// <param name="server">TimeTagger API url</param>
+        /// <param name="apiKey">TimeTagger API key</param>
         public TimeTaggerClient( string server, string apiKey )
         {
             Server = server.EndsWith( '/' ) ? server : $"{server}/";
@@ -24,6 +34,13 @@ namespace TimeTaggerClient
             HttpClient.DefaultRequestHeaders.Add( "authtoken", apiKey );
         }
 
+        /// <summary>
+        /// Fetches TimeTagger records. Returns all records that overlap range specified by <paramref name="start"/> and <paramref name="end"/> range. Deleted records have <seealso cref="TimeTaggerRecord.Description"/> staring with "HIDDEN".
+        /// </summary>
+        /// <param name="start">The start time for the TimeTagger records to fetch.</param>
+        /// <param name="end">The end time for the TimeTagger records to fetch.</param>
+        /// <returns>List of <see cref="TimeTaggerRecord"/> matching criteria</returns>
+        /// <exception cref="HttpRequestException">If unexpected status was returned by the API</exception>
         public async Task<IEnumerable<TimeTaggerRecord>> FetchRecordsAsync( DateTime? start = null, DateTime? end = null )
         {
             var startOffset = new DateTimeOffset( start ?? DateTime.UnixEpoch );
@@ -43,6 +60,11 @@ namespace TimeTaggerClient
             }
         }
 
+        /// <summary>
+        /// Fetches TimeTagger settings.
+        /// </summary>
+        /// <returns>List of <see cref="TimeTaggerSetting"/></returns>
+        /// <exception cref="HttpRequestException">If unexpected status was returned by the API</exception>
         public async Task<IEnumerable<TimeTaggerSetting>> FetchSettingsAsync()
         {
             var response = await HttpClient.GetAsync( "settings" );
@@ -60,6 +82,14 @@ namespace TimeTaggerClient
             }
         }
 
+        /// <summary>
+        /// Fetches new and changed TimeTagger records and settings since a specific datetime.
+        /// </summary>
+        /// <remarks>Is based on server time, so client should store most recent <see cref="TimeTaggerRecord.ServerTime"/> and <see cref="TimeTaggerSetting.ServerTime"/> to use as <paramref name="since"/> for next call</remarks>
+        /// <param name="since">Fetches TimeTagger records since this datetime.</param>
+        /// <returns>List of <see cref="TimeTaggerRecord"/> and <see cref="TimeTaggerSetting"/> added or modified since <paramref name="since"/></returns>
+        /// <exception cref="InvalidDataException">Unable to parse server response</exception>
+        /// <exception cref="HttpRequestException">In unexpected status was returned by the API</exception>
         public async Task<(DateTime ServerTime, IEnumerable<TimeTaggerRecord> Records, IEnumerable<TimeTaggerSetting> Settings)> FetchNewAsync( DateTime since )
         {
             var sinceOffset = new DateTimeOffset( since );
@@ -94,6 +124,14 @@ namespace TimeTaggerClient
             }
         }
 
+        /// <summary>
+        /// Update or create records on the server.
+        /// </summary>
+        /// <remarks>New records must have unique identifier set. Otherwise record with specified id will be modified.</remarks>
+        /// <param name="records">List of <see cref="TimeTaggerRecord"/>s to create/modify</param>
+        /// <returns>Tuple with list of accepted and rejected keys, and list of errors (one for each rejected key and possibly additional errors)</returns>
+        /// <exception cref="InvalidDataException">Unable to parse server response</exception>
+        /// <exception cref="HttpRequestException">In unexpected status was returned by the API</exception>
         public async Task<(IEnumerable<string> AcceptedKeys, IEnumerable<string> RejectedKeys, IEnumerable<string> Errors)> UpdateRecordsAsync( IEnumerable<TimeTaggerRecord> records )
         {
             var response = await HttpClient.PutAsync( "records", new StringContent( JsonSerializer.Serialize( records.Select( r => r.ToApiRecord() ) ), Encoding.UTF8, "application/json" ) );
@@ -110,7 +148,15 @@ namespace TimeTaggerClient
                 throw new HttpRequestException( $"Unexpected status: {response.StatusCode}\n{content}" );
             }
         }
-        
+
+        /// <summary>
+        /// Delete records from the server.
+        /// </summary>
+        /// <remarks>TimeTagger doesn't allow to really delete records. Instead records with <see cref="TimeTaggerRecord.Description"/> starting with "HIDDEN" are treated by clients as removed. This function is really a wrapper for <see cref="TimeTaggerClient.UpdateRecordsAsync(IEnumerable{TimeTaggerRecord})"/></remarks>
+        /// <param name="records">List of records to delete.</param>
+        /// <returns>Tuple with list of accepted and rejected keys, and list of errors (one for each rejected key and possibly additional errors)</returns>
+        /// <exception cref="InvalidDataException">Unable to parse server response</exception>
+        /// <exception cref="HttpRequestException">In unexpected status was returned by the API</exception>
         public async Task<(IEnumerable<string> AcceptedKeys, IEnumerable<string> RejectedKeys, IEnumerable<string> Errors)> DeleteRecordsAsync( IEnumerable<TimeTaggerRecord> records )
         {
             var recordsWithMarker = records.Select( r => r.Description?.StartsWith( "HIDDEN" ) ?? false ? r : r with { Description = $"HIDDEN {r.Description}" } );
@@ -118,7 +164,14 @@ namespace TimeTaggerClient
             return await UpdateRecordsAsync( recordsWithMarker );
         }
 
-        
+        /// <summary>
+        /// Update or create settings on the server.
+        /// </summary>
+        /// <remarks>New settings must have unique identifier set. Otherwise setting with specified id will be modified.</remarks>
+        /// <param name="settings">List of <see cref="TimeTaggerSetting"/> to create or modify on server.</param>
+        /// <returns>Tuple with list of accepted and rejected keys, and list of errors (one for each rejected key and possibly additional errors)</returns>
+        /// <exception cref="InvalidDataException">Unable to parse server response</exception>
+        /// <exception cref="HttpRequestException">In unexpected status was returned by the API</exception>
         public async Task<(IEnumerable<string> AcceptedKeys, IEnumerable<string> RejectedKeys, IEnumerable<string> Errors)> UpdateSettingsAsync( IEnumerable<TimeTaggerSetting> settings )
         {
             var response = await HttpClient.PutAsync( "settings", new StringContent( JsonSerializer.Serialize( settings.Select( r => r.ToApiSetting() ) ), Encoding.UTF8, "application/json" ) );
@@ -136,6 +189,10 @@ namespace TimeTaggerClient
             }
         }
 
+        /// <summary>
+        /// Implements cleanup logic.
+        /// </summary>
+        /// <param name="disposing"></param>
         protected virtual void Dispose( bool disposing )
         {
             if (!disposedValue)
@@ -149,6 +206,9 @@ namespace TimeTaggerClient
             }
         }
 
+        /// <summary>
+        /// Dispose object.
+        /// </summary>
         public void Dispose()
         {
             // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
